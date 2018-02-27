68bac0deb4a76c34fdbbf89bb44c2af9112691b6
Anthony Williams <anthony@anthonys-mbp.home>
2018-02-27 16:27:48 +0000

    Figuring out how to render an array of custom components (looks easy enough!)

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -5,29 +5,62 @@ import style from './canvas.css';
 import List from '../list';
 import BuilderButton from '../builder-button';
 
+const square = {
+    style: {
+        width: '10%',
+        height: '10%',
+        border_colour: '#F00'
+    },
+    id: 'square',
+    type: 'shape'
+};
+
+
 class Canvas extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			articles: props.articles
-		};
+        const shapes = [];
+        shapes.push(square);
+        shapes.push(square);
 
-		console.log('Style: ', style);
-	}
+        this.state = {
+            articles: props.articles,
+            shapes: shapes
+        };
 
-	componentWillReceiveProps(nextProps) {
-		this.setState({articles: nextProps.articles});
-	}
+        console.log('Style: ', style);
+    }
 
-	render() {
-		return (
-			<div className={style.canvas}>
-				<List />
-				<BuilderButton />
-			</div>
-		);
-	}
+    componentWillReceiveProps(nextProps) {
+        this.setState({articles: nextProps.articles});
+    }
+
+    buildComponent() {
+
+    }
+
+    buildCanvas() {
+        return [
+            <div style="position: absolute; top: 40px; left: 40px; background: red; width: 100px; height: 100px; border: solid 2px green;">hello</div>,
+            <div style="position: absolute; top: 60px; left: 160px; background: blue; width: 50px; height: 50px; border: solid 2px purple;">hello</div>,
+            <div style="position: absolute; top: 80px; left: 260px; background: orange; width: 30px; height: 30px; border: solid 2px blue;">hello</div>,
+            <BuilderButton />,
+            <BuilderButton />,
+            <BuilderButton />,
+        ];
+    }
+
+    render() {
+        let shapes = this.buildCanvas();
+        return (
+            <div className={style.canvas}>
+                {shapes}
+                <List/>
+                <BuilderButton/>
+            </div>
+        );
+    }
 }
 
 export default connect((state) => ({articles: state.articles}))(Canvas);
